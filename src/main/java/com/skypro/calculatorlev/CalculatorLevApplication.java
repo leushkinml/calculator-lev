5ea04b67c714496a690a630e898236ddff6d45ed
leush <leushkinml@mail.ru>
2022-12-02 02:39:19 +0300

    Initial commit

---
+++ src/main/java/com/skypro/calculatorlev/CalculatorLevApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.calculatorlev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorLevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorLevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/calculatorlev/CalculatorLevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.calculatorlev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorLevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
